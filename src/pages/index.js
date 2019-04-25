9c65fe4df0064cc5cba4476c4ca74cb1dead91cf
Sam Silver <samsilver@wustl.edu>
2019-04-25 00:10:27 -0700

    Testing Netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,10 @@ import styled from "styled-components"
 // import YouTube from 'react-youtube';
 // import { Player } from 'video-react';
 import { graphql } from "gatsby";
-// import { ChevronRight } from 'styled-icons/boxicons-regular/ChevronRight'
 
 
 import "../styles/scrollbar.css"
 import "../styles/index.css"
-
-// import { Link } from "gatsby"
-
-
-/*
-Styled Components lets you use actual CSS syntax inside your components. Styled Components is a variant on “CSS-in-JS”—which solves many of the problems with traditional CSS.
-One of the most important problems they solve is selector name collisions. With traditional CSS, you have to be careful not to overwrite CSS selectors used elsewhere in a site because all CSS selectors live in the same global namespace. This unfortunate restriction can lead to elaborate (and often confusing) selector naming schemes.
-*/
-
-/**
- * Okay Okay...
- * So I have the scraped data somewhere out there. JSON.
- * There's probably a JSON data plugin out there:
- * [Source] plugins “source” data from remote or local locations into what Gatsby calls nodes.
- * [Transformer] plugins “transform” data provided by source plugins into new nodes and/or node fields.
- * Check out the Plugin Library to see if the source plugins and/or transformer plugins you’d like to use already exist
- * I should deal with that later I think -- How can I mock the data now just to build my components?
- * How about let's style and build a single component here in the index.js. You can hardcode the information in it.
- * 1. Hardcode a single card here and style it.
- * 2. Move the card into a component and pass in information to it here via props
- * 3. Put the JSON data into a local MongoDB server.
- * 3. Use the plugins to read the JSON GraphQL data... use that data to generate a card for each one..
- * 4. Add some additional styling and animations
- * 5. Something about login... I dunno man.
- */
-
-// --------------
-
 
 const FakeBody = styled.div`
   width: 100%;
@@ -132,13 +103,8 @@ const Description = styled.div`
 const Info = styled.p`
   ${'' /* Nothing here now */}
 `
-
-
-
 // NEW
 const Card = (props) => {
-
-  // Manipulate the data to extract...
 
   return (
     <Container>
@@ -172,39 +138,6 @@ const Card = (props) => {
     </Container >
   )
 }
-
-
-// OLD
-// const Card = (props) => (
-//   <Container>
-//     <OrgImgContainer>
-//       <OrgImg src={props.image}></OrgImg>
-//     </OrgImgContainer>
-//     <Description className='description'>
-//       <Info>
-//         <strong>Position Title:</strong>
-//         <br></br>
-//         {props.jobName}
-//       </Info>
-//       <Info>
-//         <strong>Organization:</strong>
-//         <br></br>
-//         {props.orgName}
-//       </Info>
-//       <Info>
-//         <strong>Position Description:</strong>
-//         <br></br>
-//         {props.jobDescription}
-//       </Info>
-//       <Info>
-//         <strong>Organization Description:</strong>
-//         <br></br>
-//         {props.orgDescription}
-//       </Info>
-//     </Description>
-//   </Container >
-// )
-
 
 export const query = graphql`
 {
@@ -251,42 +184,6 @@ const PitchCard = styled.div`
   padding: 10px;
 
 `
-
-
-
-
-
-// const fakeProps = {
-//   "image": "https://80000hours.org/wp-content/uploads/2019/04/google.jpg",
-//   "jobName": "Director, Global Affairs and Public Policy, Foreign Policy Institutions",
-//   "orgName": "Google",
-//   "jobDescription": "You'll design and lead Google's engagement with US Government agencies charged with the administration of US foreign policy, foreign embassies based in the US, and international institutions based in the US. You will coordinate with other members of the global Government Affairs & Public Policy team and key cross-functional partners to further Google’s public policy agenda globally and educate US-based policymakers on Google’s global activities. You will liaise with relevant internationally-focused trade associations, think tanks, and other key opinion-formers most relevant to these institutions and issues.",
-//   "orgDescription": "Google’s parent company, Alphabet, is one of the world’s largest information technology company. Its core search engine, Google.com, is the internet’s most visited website and it houses Google Brain, one of the world’s top deep learning labs. Google has published principles committing it to making progress in the responsible development of AI, helping answer questions about the best way to build fairness, interpretability, privacy, and security into AI systems and sharing knowledge, research, tools, datasets, and other resources with the larger community."
-// }
-// const fakeProps2 = {
-//   "image": "https://cdn.80000hours.org/wp-content/uploads/2017/03/openAI_logo2-150x150.png",
-//   "jobName": "Strategic Finance Lead",
-//   "orgName": "OpenAI",
-//   "jobDescription": "OpenAI’s Finance team is responsible for ensuring the organization is set up for long-term success in pursuit of its mission. You will directly contribute to this initiative by owning all aspects of our financial models, analyzing and recommending strategic investments, and providing senior management and the Board with actionable insight into the state of our operations. Responsibilities will include building and maintaining OpenAI’s operating model and budget, analyzing, recommending, and overseeing large strategic projects and investments, and supporting fundraising processes and managing relationships with investors.",
-//   "orgDescription": "OpenAI is a nonprofit research company, founded in 2015 with the goal of discovering and enacting the path to safe artificial general intelligence. It has received $1 billion in funding commitments from the technology community, and is one of the leading organisations working on general AI development."
-// }
-// const fakeProps3 = {
-//   "image": "https://80000hours.org/wp-content/uploads/2018/08/CSERlogo.jpg",
-//   "jobName": "Research Associate in Global Justice and Global Catastrophic Risks",
-//   "orgName": "Centre for the Study of Existential Risk",
-//   "jobDescription": "This research programme seeks to study ways in which global inequality and injustice increase vulnerability to global catastrophic and existential risks, work in collaboration with partners to incorporate population and economic growth, resource scarcity, and global equity into the overall evaluation of global policy and existential risk, and develop a practical framework for global catastrophe justice designed to complement existing guidance such as the Sendai Framework for Disaster Risk Reduction and the Sustainable Development Goals.",
-//   "orgDescription": "CSER is an interdisciplinary research centre at the University of Cambridge dedicated to the study and mitigation of existential risks. Their research focuses on biological risks, environmental risks, risks from artificial intelligence, and how to manage extreme technological risk in general. It was founded in 2012 by Professor Lord Martin Rees, the Astronomer Royal, Jaan Tallinn, the co-founder of Skype, and Huw Price, the Bertrand Russell Professor of Philosophy."
-// }
-// const fakeProps4 = {
-//   "image": "https://cdn.80000hours.org/wp-content/uploads/2018/05/deepmind-150x150.png",
-//   "jobName": "Recruiter, Engineering",
-//   "orgName": "DeepMind",
-//   "jobDescription": "Your responsibilities will include helping organize and launch candidate searches and build initial lists of potential candidates, working closely with and offering expert advice to engineering and recruiting managers to build talented and diverse candidate pipelines, developing research strategies, conducting in-depth searches, and providing shortlists of qualified engineering candidates to the recruitment team and hiring managers.",
-//   "orgDescription": "DeepMind is probably the largest and most advanced research group developing general machine intelligence. It includes an ethics & society team\n and has published a number of research papers and blog posts on safety issues."
-// }
-
-
-
 
 export default ({ data }) => {
 
